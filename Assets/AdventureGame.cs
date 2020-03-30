03333cd1e72aa35631267441900cc50907fde3d6
Riku Kurjenrauma <riku.kurjenrauma@gmail.com>
2020-03-30 05:45:34 +0300

    Added the game master

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+	// Variables
+	[SerializeField] Text textComponent;
+
+	// Internal Variables
+
+
+	// Debug
+
+
+	// Start is called before the first frame update
+	void Start() {
+		textComponent.text = "Are you ready to start your adventure?";
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
