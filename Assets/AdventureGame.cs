fdca1b82d2031fc5ad637592de572b91e948f417
Riku Kurjenrauma <riku.kurjenrauma@gmail.com>
2020-03-30 08:06:49 +0300

    Added game state system

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,20 +6,36 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 	// Variables
 	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
 
 	// Internal Variables
-
+	State currentState;
+	State previousState;
 
 	// Debug
 
 
 	// Start is called before the first frame update
 	void Start() {
-		textComponent.text = "Are you ready to start your adventure?";
+		currentState = startingState;
+		textComponent.text = currentState.GetStateStory();
 	}
 
 	// Update is called once per frame
 	void Update() {
-		
+		ManageState();
+		textComponent.text = currentState.GetStateStory();
+	}
+
+	void ManageState() {
+		State[] nextStates = currentState.GetNextStates();
+
+		if(Input.GetKeyDown(KeyCode.Alpha1)) {
+			currentState = nextStates[0];
+		}
+
+		if(Input.GetKeyDown(KeyCode.Alpha2)) {
+			currentState = nextStates[1];
+		}
 	}
 }
---
+++ Assets/State.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+	// Variables
+	[TextArea(2, 20)] [SerializeField] string titleText;
+	[TextArea(20, 20)] [SerializeField] string storyText;
+	[SerializeField] State[] nextStates;
+
+	// Internal Variables
+
+
+	// Debug
+
+	public string GetStateStory(){
+		return storyText;
+	}
+
+	public string GetStateTitle(){
+		return titleText;
+	}
+
+	public string[] GetAllStateText(){
+		string[] stateTexts = new string[2];
+		stateTexts[0] = titleText;
+		stateTexts[1] = storyText;
+		return stateTexts;
+	}
+
+	public State[] GetNextStates() {
+		return nextStates;
+	}
+}
